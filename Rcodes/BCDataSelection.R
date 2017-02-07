6274b1b6e614ce6037d62e6e6eaa485d95b48c73
Yong Luo <yluo1@lakeheadu.ca>
2017-02-07 01:19:23 -0800

    update bc data selection

--- Rcodes/BCDataSelection.R
+++ Rcodes/BCDataSelection.R
@@ -1,6 +1,7 @@
 # get the BC data
 rm(list = ls())
-library(data.table);library(ggplot2);library(gridExtra)
+library(data.table);library(ggplot2);library(gridExtra);library(raster)
+library(dplyr)
 workPath <- "~/Github/Species-function-Tree-growth-Climate-change"
 BCPlotInfor <- fread(file.path(workPath,"data", "BC", "StandInformation.csv"))
 print(length(unique(BCPlotInfor$SAMP_ID)))
@@ -77,7 +78,7 @@ treesInSelctedPlots <- treesInSelctedPlots[!(uniTreeID %in% noMappingTrees), ]
 # check tree had different angle but all had values
 print(setkey(treesInSelctedPlots[minAngle != maxAngle,.(uniTreeID, meas_yr, 
                                                         x_coord, y_coord)],
-      uniTreeID, meas_yr))
+             uniTreeID, meas_yr))
 
 # uniTreeID meas_yr    x_coord    y_coord
 # 1: 69018 G000010_01_0041_1    1996 -3.1761477 -0.3899819
@@ -187,16 +188,23 @@ mappingCheck[, subplottrees:=factor(sub_plot_tree, levels = c("N", "Y", ""),
                                     labels = c("In main plot", "In sub plot", "Unknown"))]
 points360 <- seq(0, 2 * pi, length.out = 360)
 treeplot <- Polygons(list(Polygon(cbind(5.64 * sin(points360),
-                                         5.64 * cos(points360)))),
-                      ID = 1)
+                                        5.64 * cos(points360)))),
+                     ID = 1)
 treeplot <- SpatialPolygons(list(treeplot))
 treeplot <- fortify(treeplot, region = "ID") %>% data.table
 
 ecologicalplot <- Polygons(list(Polygon(cbind(10 * sin(points360),
-                                        10 * cos(points360)))),
-                     ID = 1)
+                                              10 * cos(points360)))),
+                           ID = 1)
 ecologicalplot <- SpatialPolygons(list(ecologicalplot))
 ecologicalplot <- fortify(ecologicalplot, region = "ID") %>% data.table
+
+transectionRange <- Polygons(list(Polygon(cbind(24 * sin(points360),
+                                              24 * cos(points360)))),
+                           ID = 1)
+transectionRange <- SpatialPolygons(list(transectionRange))
+transectionRange <- fortify(transectionRange, region = "ID") %>% data.table
+
 
 successionplot <- Polygons(list(Polygon(cbind(25 * sin(points360),
                                               25 * cos(points360)))),
@@ -215,8 +223,10 @@ a <- ggplot(data = mappingCheck,
   geom_path(data = treeplot, aes(x = long, y = lat, group = group), col = "blue", linetype = 1)+
   geom_path(data = ecologicalplot, aes(x = long, y = lat, group = group), 
             col = "blue", linetype = 2)+
-  geom_path(data = successionplot, aes(x = long, y = lat, group = group), 
+  geom_path(data = transectionRange, aes(x = long, y = lat, group = group), 
             col = "blue", linetype = 3)+
+  geom_path(data = successionplot, aes(x = long, y = lat, group = group), 
+            col = "blue", linetype = 4)+
   scale_color_manual(name = "Trees location", values = c("gray", "red", "green"))+
   scale_x_continuous(name = "x", limits = c(-60, 60))+
   scale_y_continuous(name = "y", limits = c(-60, 60))+
@@ -234,19 +244,21 @@ print(range(treesInSelctedPlots$dbh))
 
 treesInSelctedPlots[, NumberOfPlot:=length(unique(plot_no)), by = SAMP_ID]
 mutiplesubplots <- unique(treesInSelctedPlots[NumberOfPlot>1,]$SAMP_ID)
+oneplots <- unique(treesInSelctedPlots[NumberOfPlot == 1,]$SAMP_ID)
 allMultiPlots <- list()
 for(i in 1:length(mutiplesubplots)){
   allMultiPlots[[i]] <- ggplot(data = mappingCheck[SAMP_ID == mutiplesubplots[i],], 
-            aes(x = x_coord, y = y_coord))+
-  geom_point(aes(col = plot_no))+
-  scale_x_continuous(name = "x")+
-  scale_y_continuous(name = "y")+
+                               aes(x = x_coord, y = y_coord))+
+    geom_point(aes(col = plot_no))+
+    geom_path(data = treeplot, aes(x = long, y = lat, group = group), col = "blue", linetype = 1)+
+    scale_x_continuous(name = "x")+
+    scale_y_continuous(name = "y")+
     annotate("text", x = -Inf, y = Inf, label = mutiplesubplots[i], hjust = -0.5,
              vjust = 1.5)+
-  theme_bw()+
-  theme(panel.grid = element_blank(),
-        panel.background = element_blank(),
-        legend.position = "none")
+    theme_bw()+
+    theme(panel.grid = element_blank(),
+          panel.background = element_blank(),
+          legend.position = "none")
 }
 
 plotlayout <- rbind(c(1,NA), c(2, 3), c(4, 5))
@@ -256,3 +268,60 @@ b <- grid.arrange(allMultiPlots[[1]], allMultiPlots[[2]], allMultiPlots[[3]],
 ggsave(file.path(workPath, "tableFigures", "sampleIDhadsubplots.png"),
        b, height = 10, width = 7)
 
+
+
+onePlotMapping <- list()
+for(i in oneplots){
+  onePlotMapping[[i]] <- ggplot(data = mappingCheck[SAMP_ID == i,], 
+                           aes(x = x_coord, y = y_coord))+
+    geom_point()+
+    geom_path(data = treeplot, aes(x = long, y = lat, group = group), col = "blue", linetype = 1)+
+    scale_x_continuous(name = "x")+
+    scale_y_continuous(name = "y")+
+    annotate("text", x = -Inf, y = Inf, label = i, hjust = -0.5,
+             vjust = 1.5)+
+    theme_bw()+
+    theme(panel.grid = element_blank(),
+          panel.background = element_blank(),
+          legend.position = "none")
+  
+}
+
+for(i in 1:length(oneplots)){
+  ggsave(file.path(workPath, "tableFigures", 
+                   paste("singleplotMapping", i, ".png", sep = "")),
+         onePlotMapping[[i]], height = 7, width = 7)
+}
+
+unique(selectedPlots$sampletype) # "G" "R" "I"
+for(j in c("G", "R", "I")){
+  thePlots <- unique(selectedPlots[sampletype == j]$SAMP_ID)
+  GrowthSamplingplots <- ggplot(data = mappingCheck[SAMP_ID %in% thePlots,], 
+                                aes(x = x_coord, y = y_coord))+
+    geom_point(data = mappingCheck[SAMP_ID %in% thePlots &
+                                     subplottrees == "In main plot",],
+               aes(col = subplottrees))+
+    geom_point(data = mappingCheck[SAMP_ID %in% thePlots & 
+                                     subplottrees == "In sub plot",],
+               aes(col = subplottrees))+
+    geom_point(data = mappingCheck[SAMP_ID %in% thePlots & 
+                                     subplottrees == "Unknown",],
+               aes(col = subplottrees))+
+    geom_path(data = treeplot, aes(x = long, y = lat, group = group),
+              col = "blue", linetype = 1)+
+    scale_color_manual(name = "Trees location", values = c("gray", "red", "green"))+
+    scale_x_continuous(name = "x", limits = c(-60, 60))+
+    scale_y_continuous(name = "y", limits = c(-60, 60))+
+    theme_bw()+
+    theme(panel.grid = element_blank(),
+          panel.background = element_blank(),
+          legend.background = element_rect(colour = "black"),
+          legend.position = c(0.9, 0.9))
+  
+  ggsave(file.path(workPath, "tableFigures", paste("alltreemapping_",
+                                                   j, ".png", sep = "")),
+         GrowthSamplingplots, height = 9, width = 9)
+  
+  
+  
+}
